8dc2999a258cfd42ebd29a611a7886aca966d8ab
Lorenzo <164573905+lobrug@users.noreply.github.com>
2026-07-01 16:14:26 -0300

    Add function declaration for enviarFrame_ManchesterDiferencial
    Added function declaration for sending Manchester differential frames.

--- TX/SRC/manchester_diff/manchester_diff.h
+++ TX/SRC/manchester_diff/manchester_diff.h
@@ -1,3 +1,6 @@
-﻿// Contrato para o módulo manchester_diff
-// Insira aqui as assinaturas das funções conforme Contratos.md
+#ifndef MANCHESTER_DIFF_H
+#define MANCHESTER_DIFF_H
 
+void enviarFrame_ManchesterDiferencial(const uint8_t* frame, uint8_t tam, int pin_led, unsigned long tempo_bit);
+
+#endif // MANCHESTER_DIFF_H
