0ce332b67b98c34e18e98c16f368039e22c76e27
Lorenzo <164573905+lobrug@users.noreply.github.com>
2026-07-01 22:45:23 -0300

    Add receberFrame_NRZL function signature
    Added function signature for receberFrame_NRZL.

--- RX/SRC/nrz_l/nrz_l.h
+++ RX/SRC/nrz_l/nrz_l.h
@@ -1,3 +1,6 @@
-﻿// Contrato para o módulo nrz_l
-// Insira aqui as assinaturas das funções conforme Contratos.md
+#ifndef NRZL_H
+#define NRZL_H
 
+bool receberFrame_NRZL(uint8_t* frame_out, uint8_t* tam_lido, int pin_sensor, unsigned long tempo_bit);
+
+#endif // NRZL_H
