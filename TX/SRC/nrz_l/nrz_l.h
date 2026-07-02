f81f01bd23d79df6774281ba65c69e34156f037d
Lorenzo <164573905+lobrug@users.noreply.github.com>
2026-07-01 22:44:05 -0300

    Add function prototype for enviarFrame_NRZL
    Added function prototype for enviarFrame_NRZL.

--- TX/SRC/nrz_l/nrz_l.h
+++ TX/SRC/nrz_l/nrz_l.h
@@ -1,3 +1,6 @@
-﻿// Contrato para o módulo nrz_l
-// Insira aqui as assinaturas das funções conforme Contratos.md
+#ifndef NRZ_L_H
+#define NRZ_L_H
 
+void enviarFrame_NRZL(const uint8_t* frame, uint8_t tam, int pin_led, unsigned long tempo_bit);
+
+#endif // NRZ_L_H
